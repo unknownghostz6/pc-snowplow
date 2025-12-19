c6451bec637680241db9d1478358bc894cd61bf2
unknownghostz6 <88034715+unknownghostz6@users.noreply.github.com>
2025-12-19 15:17:16 -0700

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,6 +5,15 @@ fx_version 'cerulean'
 game 'gta5'
 lua54 'yes'
 
+author 'crusopaul (updated by UnknownGhostz for latest qb-core compatibility)'
+
+dependencies {
+    'qb-core',
+    'qb-target',
+    'qb-menu',
+    'qb-weathersync'
+}
+
 shared_script 'config.lua'
 
 server_scripts {
